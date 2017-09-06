528994da8b79a5f154c8575ed7f14c5b985e462c
pashkoAntonO <anton.pashko.ol@gmail.com>
2017-09-06 11:26:30 +0300

    ready

---
+++ InsertSort/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertSort
+{
+    class Program
+    {
+
+        public static void insertSort(ref int [] array)
+        {
+
+            int swap;
+            for (int i = 1; i < array.Length; i++)
+            {
+
+                for (int j = i; j > 0; j--)
+                {
+                    if(array[j-1] > array[j])
+                    {
+                        swap = array[j - 1];
+                        array[j - 1] = array[j];
+                        array[j] = swap;
+                    }
+
+                }
+
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            int[] array = new int[] { 5, 15, 15, 24, 11, 55, 4, 1, 74, 157, 23 };
+
+            insertSort(ref array);
+
+            foreach (int elem in array)
+            {
+                Console.WriteLine(elem);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
